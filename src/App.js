4f1ef9f16ca0b2e7b93903cd16a3cc171e50a24e
Marco <jonghyeok.lim14@gmail.com>
2022-08-09 20:06:13 +0900

    components

--- src/App.js
+++ src/App.js
@@ -15,17 +15,6 @@ function App() {
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
-
-      <button
-        onClick={() => {
-          let copy = [...글제목];
-          copy[0] = "여자코트 추천";
-          글제목변경(copy);
-        }}
-      >
-        글수정
-      </button>
-
       <div className="list">
         <h4>
           {글제목[0]}
@@ -48,6 +37,17 @@ function App() {
         <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+      <Modal />
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
